9a7b2ee5164b6c6f2aca6090aa8bdee782061a03
Charlie-Foy-117 <83784786+Charlie-Foy-117@users.noreply.github.com>
2022-09-18 14:36:59 +0100

    1-3 problems complete and problem 4 started

--- HomeworkPracticeQuestions_WeekTwo/HomeworkPracticeQuestions_WeekTwo/HomeworkPracticeQuestions_WeekTwo.cpp
+++ HomeworkPracticeQuestions_WeekTwo/HomeworkPracticeQuestions_WeekTwo/HomeworkPracticeQuestions_WeekTwo.cpp
@@ -2,10 +2,85 @@
 //
 
 #include <iostream>
+#include <string>
+
+int RecAreaCalc()
+{
+    int width = 0;
+    int length = 0; 
+    int area = 0;
+    std::cout << "Please enter the rectangles length" << std::endl;
+    std::cin >> length;
+    std::cout << "Please enter the rectangle width" << std::endl;
+    std::cin >> width;
+    area = width * length;
+    return area;
+}
+
+int RandScore()
+{
+    
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //problem one
+    /*
+    //variables
+    std::string playerWord;
+    std::string badWord = "bad";
+    std::string fillerWord = "BLEEP";
+
+    std::cout << "Please enter a word" << std::endl;
+    std::cin >> playerWord;
+
+    //output
+    if (playerWord == badWord)
+    {
+        std::cout << "You entered " << fillerWord << std::endl;
+    }
+    else
+    {
+        std::cout << "You entered " << playerWord << std::endl;
+    }
+    */
+
+    //problem two
+    /*
+    //variables
+    int numberOfBottles = 99;
+    std::string playerChoice;
+    std::string replay;
+
+    do
+    {
+        //input
+        std::cout << "There are " << numberOfBottles << " bottles on the wall" << std::endl
+            << "Would you like to drink one - yes(y) or no(n)" << std::endl;
+        std::cin >> playerChoice;
+
+        if (playerChoice == "y")
+        {
+            --numberOfBottles;
+            std::cout << numberOfBottles << " bottles of beer on the wall" << std::endl;
+            
+        }
+        if (numberOfBottles == 0)
+        {
+            std::cout << "No more bottles on the wall" << std::endl;
+            playerChoice = "n";
+        }
+    } while (playerChoice == "y");
+    */
+
+    //problem three
+    /*
+    std::cout << "The area of the rectangle is " << RecAreaCalc() << std::endl;
+    */
+
+    //problem four
+
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
